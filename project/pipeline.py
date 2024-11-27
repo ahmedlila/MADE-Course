8f788765ce3d6b0a26600830948298df8206c6aa
Ahmed Lila <68854086+ahmedlila@users.noreply.github.com>
2024-11-27 19:55:01 +0100

    refactor: update pipeline

--- project/pipeline.py
+++ project/pipeline.py
@@ -17,9 +17,12 @@ data_files = {
 
 world_bank_url = "https://api.worldbank.org/v2/en/indicator/SP.DYN.LE00.IN?downloadformat=csv"
 specific_file = 'API_SP.DYN.LE00.IN_DS2_en_csv_v2_99.csv'
-dropped_cols = ["IND_ID", "IND_CODE", "IND_UUID", "IND_PER_CODE", "DIM_TIME_TYPE", "DIM_GEO_CODE_M49", "DIM_GEO_CODE_TYPE", "DIM_PUBLISH_STATE_CODE"]
+UNNECESSARY_COLS = ["IND_ID", "IND_CODE", "IND_UUID", "IND_PER_CODE", "DIM_TIME_TYPE", "DIM_GEO_CODE_M49", "DIM_GEO_CODE_TYPE", "DIM_PUBLISH_STATE_CODE"]
+SHEETS_ABBREVIATIONS  = ['LIFE_EXPECTANCY','HYPERTENSION', 'UHC', 'DTP3', 'MVC2']
 output_dir = "data/"
 
+#############################################
+### Fetching Datasets
 
 def download_world_bank_data(url: str, specific_file: str, output_dir: str) -> pd.DataFrame:
     """
@@ -44,18 +47,19 @@ def download_world_bank_data(url: str, specific_file: str, output_dir: str) -> p
         return df
 
 
-def process_world_bank_data(df: pd.DataFrame, country: str) -> pd.DataFrame:
+# Preprocessing
+def preprocess_world_bank_data(df: pd.DataFrame, country: str) -> pd.DataFrame:
     """
-    Processes the World Bank data for a specific country.
-
+    Preprocesses the World Bank data for a specific country.
+    
     Args:
-        df (pd.DataFrame): The DataFrame containing the World Bank data.
+        df (pd.DataFrame): The DataFrame to preprocess.
         country (str): The country to filter the data for.
-
+    
     Returns:
-        pd.DataFrame: The processed data with life expectancy for the specified country.
+        pd.DataFrame: The preprocessed DataFrame.
     """
-    wb_life_expectancy = df[df['Country Name'] == country].iloc[:, 4:].T # skip first 4 columns
+    wb_life_expectancy = df[df['Country Name'] == country].iloc[:, 4:].T  # Skip first 4 columns
     wb_life_expectancy.dropna(inplace=True)
     wb_life_expectancy.reset_index(inplace=True)
     wb_life_expectancy.columns = ["DIM_TIME", 'Life Expectancy']
@@ -74,23 +78,80 @@ def read_and_filter_csv(url: str, country: str) -> pd.DataFrame:
         pd.DataFrame: The filtered DataFrame.
     """
     df = pd.read_csv(url)
-    return df[df["GEO_NAME_SHORT"] == country]
+    country_df = df[df["GEO_NAME_SHORT"] == country]
+    country_df = country_df.drop(columns=UNNECESSARY_COLS)
+    return country_df
 
-def clean_columns(df: pd.DataFrame, dropped_cols: list) -> pd.DataFrame:
+def preprocess_and_rename(df: pd.DataFrame, prefix: str) -> pd.DataFrame:
+    df = df.drop(columns=['IND_NAME', 'GEO_NAME_SHORT'], errors='ignore')
+    df = df.rename(columns={col: f"{prefix}_{col}" for col in df.columns if col != 'DIM_TIME'})
+    return df
+
+
+# Analysis
+def check_duplicates_and_nulls(df: pd.DataFrame, name: str):
     """
-    Cleans the DataFrame by dropping specified columns.
+    Checks for duplicates and null values in a DataFrame.
+    """
+    print(f"{name} - Null Values: {df.isnull().sum().sum()}, Duplicates: {df.duplicated().sum()}")
 
+def show_descriptive_stats(df: pd.DataFrame, name: str):
+    """
+    Shows descriptive statistics for a DataFrame.
+    """
+    print(f"Descriptive Statistics for {name}:\n{df.describe(include='all')}")
+
+
+# Cleaning
+def clean_merged_data(merged_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans a merged DataFrame by dropping rows with all missing values and sorting it by the columns.
+    
     Args:
-        df (pd.DataFrame): The DataFrame to clean.
-        dropped_cols (list): The list of columns to drop.
-
+        merged_df (pd.DataFrame): The merged DataFrame to clean.
+    
     Returns:
         pd.DataFrame: The cleaned DataFrame.
     """
-    df.drop(columns=dropped_cols, inplace=True)
+    merged_df_cleaned = merged_df.dropna(how='all', 
+                                         subset=['DTP3_RATE_PER_100_N', 
+                                                 'MVC2_RATE_PER_100_N', 
+                                                 'HYPERTENSION_RATE_PER_100_N', 
+                                                 'UHC_INDEX_N'])
+    merged_df_cleaned = merged_df_cleaned.sort_values(by=list(merged_df.columns))
+    return merged_df_cleaned
+
+# Imputation
+def impute_missing_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Imputes missing data in a DataFrame.
+    """
+    df['DTP3_RATE_PER_100_N'] = df['DTP3_RATE_PER_100_N'].interpolate(method='linear')
+    df['MVC2_RATE_PER_100_N'] = df['MVC2_RATE_PER_100_N'].interpolate(method='linear')
+    df['UHC_INDEX_N'] = df['UHC_INDEX_N'].interpolate(method='linear')
+    df['HYPERTENSION_RATE_PER_100_N'] = df[['HYPERTENSION_RATE_PER_100_NL', 'HYPERTENSION_RATE_PER_100_NU']].mean(axis=1)
+    df.fillna(method='ffill', inplace=True)
+    df.fillna(method='bfill', inplace=True)
     return df
 
-def merge_dataframes(dfs: list) -> pd.DataFrame:
+
+def preprocess_and_rename(df, prefix):
+    """
+    Preprocesses and renames a DataFrame for merging.
+    
+    Args:
+        df (pd.DataFrame): The DataFrame to preprocess.
+        prefix (str): The prefix to add to the column names.
+    
+    Returns:
+        pd.DataFrame: The preprocessed DataFrame.
+    """
+    df = df.drop(columns=['IND_NAME', 'GEO_NAME_SHORT'], errors='ignore')
+    df = df.rename(columns={col: f"{prefix}_{col}" for col in df.columns if col != 'DIM_TIME'})
+    return df
+
+# Transformation
+def merge_dataframes(dfs) -> pd.DataFrame:
     """
     Merges multiple DataFrames on the 'DIM_TIME' column.
 
@@ -100,74 +161,44 @@ def merge_dataframes(dfs: list) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The merged DataFrame.
     """
-    merged_df = dfs[0]
-    for df in dfs[1:]:
-        merged_df = merged_df.merge(df, on=['DIM_TIME'], how='outer')
+    wb_life_expectancy, *_ = dfs
+    merged_df = wb_life_expectancy
+    for processed_df in dfs[1:]:
+        merged_df = merged_df.merge(processed_df, on=['DIM_TIME'], how='outer')
+
     return merged_df
 
-def remove_unwanted_columns(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Removes unwanted columns from the DataFrame.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to clean.
-
-    Returns:
-        pd.DataFrame: The cleaned DataFrame.
-    """
-    columns_to_remove = [col for col in df.columns if col.startswith('IND_NAME') or col.startswith('GEO_NAME_SHORT')]
-    return df.drop(columns=columns_to_remove)
-
-def save_to_sqlite(df: pd.DataFrame, country: str) -> None:
-    """
-    Saves the DataFrame to an SQLite database.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to save.
-        country (str): The country name to use for the database file and table.
-
-    Returns:
-        None
-    """
-    conn = sqlite3.connect(f"{output_dir}{country}.db")
-    df.to_sql(f"{country}", conn, if_exists="replace", index=False)
-    conn.close()
 
 def main_pipeline(country: str, wb_country: str) -> None:
-    """
-    Main pipeline function to process and merge data for a specific country.
-
-    Args:
-        country (str): The country name to filter the data for.
-        wb_country (str): The country name to filter the World Bank data for.
-
-    Returns:
-        None
-    """
     life_expectancy_wb_df = download_world_bank_data(world_bank_url, specific_file, output_dir)
-    wb_life_expectancy = process_world_bank_data(life_expectancy_wb_df, wb_country)
+    wb_life_expectancy = preprocess_world_bank_data(life_expectancy_wb_df, wb_country)
 
     dtp3_df = read_and_filter_csv(data_files["dtp3_immunization.csv"], country)
     hypertension_df = read_and_filter_csv(data_files["hypertension_prevalence.csv"], country)
     uhc_index_df = read_and_filter_csv(data_files["uhc_index_score.csv"], country)
     mvc2_df = read_and_filter_csv(data_files["mvc2_immunization.csv"], country)
-
-    hypertension_df = clean_columns(hypertension_df, dropped_cols)
-    dtp3_df = clean_columns(dtp3_df, dropped_cols)
-    uhc_index_df = clean_columns(uhc_index_df, dropped_cols)
-    mvc2_df = clean_columns(mvc2_df, dropped_cols)
-
-    merged_df = merge_dataframes([hypertension_df, uhc_index_df, dtp3_df, mvc2_df, wb_life_expectancy])
-
-    merged_df_cleaned = remove_unwanted_columns(merged_df)
-
-    merged_df_cleaned = merged_df_cleaned.dropna().sort_values(by='DIM_TIME').reset_index(drop=True)
-    merged_df_cleaned.to_csv(f"{output_dir}{country}.csv", index=False)
-    print(f"Data for {country} merged and saved as CSV successfully.")
     
-    # Save as SQLite
-    save_to_sqlite(merged_df_cleaned, country)
-    print(f"Data for {country} merged and saved as SQLite successfully.")
+    dfs = [wb_life_expectancy, hypertension_df, uhc_index_df, dtp3_df, mvc2_df]
+    for i, (df, name) in enumerate(zip(dfs, SHEETS_ABBREVIATIONS)):
+        if name == 'LIFE_EXPECTANCY':
+            processed_df = df
+        else:
+            processed_df = preprocess_and_rename(df, name)
+        if name == 'HYPERTENSION':
+            processed_df = processed_df[~processed_df['HYPERTENSION_DIM_SEX'].isin(['MALE', 'FEMALE'])]
+            processed_df = processed_df.drop(columns=['HYPERTENSION_DIM_SEX'])
+        dfs[i] = processed_df
+        check_duplicates_and_nulls(processed_df, name)
+        show_descriptive_stats(processed_df, name)
+
+    merged_df = merge_dataframes(dfs)
+    merged_df_cleaned = clean_merged_data(merged_df)
+    merged_df_imputed = impute_missing_data(merged_df_cleaned)
+
+    # Save to CSV
+    merged_df_imputed.to_csv(f"{output_dir}{country}.csv", index=False)
+    print(f"Data for {country} merged and saved as CSV successfully.")
+
 
 if __name__ == "__main__":
     main_pipeline("Brazil", "Brazil")
